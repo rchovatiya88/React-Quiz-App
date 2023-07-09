d1e3703b19877d13e27689ed4213036a95ee498b
rchovatiya88 <rchovatiya88@gmail.com>
2023-07-09 12:30:36 -0400

    add basic tests

--- cypress/e2e/reactquiz.cy.js
+++ cypress/e2e/reactquiz.cy.js
@@ -1,12 +1,25 @@
 describe('Learn React Quiz Tests', () => {
-    // before(() => {
-
-    // });
 
     it('Lets Start the Quiz', () => {
         cy.visit('http://localhost:3000/')
         cy.get('.col-lg-8 > .btn').click();
         cy.get('.card > :nth-child(2) > :nth-child(2)').click(); // Click on the Wrong Answer
-        cy.get('')
+        cy.get('#root > section:nth-child(2) > div > div > div > div > button').click();
+        cy.get('.card > :nth-child(2) > :nth-child(1)').click();
+        cy.get('#root > section:nth-child(2) > div > div > div > div > button').click();
+        cy.get('.card > :nth-child(2) > :nth-child(1)').click();
+        cy.get('#root > section:nth-child(2) > div > div > div > div > button').click();
+        cy.get('.card > :nth-child(2) > :nth-child(1)').click();
+        cy.get('#root > section:nth-child(2) > div > div > div > div > button').click();
+        cy.get('.card > :nth-child(2) > :nth-child(1)').click();
+        cy.get('#root > section:nth-child(2) > div > div > div > div > button').click();
+        cy.get('.card > :nth-child(2) > :nth-child(1)').click();
+        cy.get('#root > section:nth-child(2) > div > div > div > div > button').click();
+        cy.get('.card > :nth-child(2) > :nth-child(1)').click();
+        cy.get('#root > section:nth-child(2) > div > div > div > div > button').click();
+        cy.get('.card > :nth-child(2) > :nth-child(1)').click();
+        cy.get('#root > section:nth-child(2) > div > div > div > div > button').click();
+        cy.get('.card > :nth-child(2) > :nth-child(1)').click();
+        cy.get('#root > section:nth-child(2) > div > div > div > div > button').click();
     });
 })
